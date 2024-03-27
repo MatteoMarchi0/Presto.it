b266d4122d274a0798c37bba94bcb80a6bb7ef00
Matteo Marchi <matteo.marchi022@gmail.com>
2024-03-27 15:33:17 +0100

    aggiornati link footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,18 +4,18 @@
         <div class="col-3 text-center d-flex flex-column justify-content-center align-items-center">
             <p class="display-6 mb-0">Presto.it</p>
             <div class="myicons-footer">
-                <a href="https://www.linkedin.com/school/aulab-srl/?originalSubdomain=it" class="p-3 icon-footer" id="linkedin">
+                <a href="https://www.linkedin.com/in/matteo-marchi-dev/" class="p-3 icon-footer" id="linkedin">
                     <i class="bi bi-linkedin"></i>
                 </a>
-                <a href="https://github.com/Hackademy-134/Presto2-Acodisti-Anonimi" class="p-3 icon-footer" id="github">
+                <a href="https://github.com/MatteoMarchi0?tab=repositories" class="p-3 icon-footer" id="github">
                     <i class="bi bi-github"></i>
                 </a>
-                <a href="https://shorturl.at/bmuwG" class="p-3 icon-footer" id="google">
+                {{-- <a href="https://shorturl.at/bmuwG" class="p-3 icon-footer" id="google">
                     <i class="bi bi-google"></i>
-                </a>
-                <a href="https://www.youtube.com/@aulab" class="p-3 icon-footer" id="youtube">
+                </a> --}}
+                {{-- <a href="https://www.youtube.com/@aulab" class="p-3 icon-footer" id="youtube">
                     <i class="bi bi-youtube"></i>
-                </a>
+                </a> --}}
             </div>
         </div>
         {{-- RICONOSCIMENTI --}}
